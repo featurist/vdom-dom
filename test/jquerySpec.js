21ccaaa9ca9e39bc0be756adcc2f6bc47823d09f
Josh Chisholm <joshuachisholm@gmail.com>
2017-02-16 07:22:09 +0000

    jQuery like-for-like tests

---
+++ test/jquerySpec.js
@@ -0,0 +1,59 @@
+// loading jquery binds it on to a global `window` if there is one. In electron,
+// there is a global `window`, but we want to run the same specs like-for-like
+// against the electron window and the virtual window. So we load a slightly
+// hacked jquery, that doesn't try anything global...
+const jquery = require('./support/jquery-commonjs')
+
+const h = require('virtual-dom/h')
+const assert = require('assert')
+
+const vdomDom = require('..')
+const VWindow = require('../vwindow')
+const VDocument = require('../vdocument')
+const VElement = require('../velement')
+const VLocation = require('../vlocation')
+
+const globalIt = global.it
+const it = function (description, callback) {
+  if (callback.length < 1) throw new Error('it expects arity 1')
+
+  if (typeof(window) !== 'undefined') {
+    globalIt(description, function () {
+      callback(jquery(global.window))
+    })
+  }
+
+  globalIt(description + ' (VDOM)', function () {
+    const html = h('html', {}, [h('body')])
+    const document = new VDocument(new VElement(html))
+    const location = new VLocation('http://example.com')
+    const window = new VWindow(document, location)
+    callback(jquery(window))
+  })
+}
+
+describe('jquery', function() {
+  describe('.html(innerHTML)', function () {
+    it('writes a single node', function ($) {
+      $('body').html('<p>Howdy</p>')
+      assert.equal($('p').length, 1)
+    })
+
+    it('writes multiple nodes', function ($) {
+      $('body').html('<i>Howdy</i><i>Doody</i>')
+      assert.equal($('i').length, 2)
+    })
+  })
+
+  describe('.html()', function () {
+    it('reads a single node', function ($) {
+      $('body').html('<p>Howdy</p>')
+      assert.equal($('body').html(), '<p>Howdy</p>')
+    })
+
+    it('writes multiple nodes', function ($) {
+      $('body').html('<i>Howdy</i><i>Doody</i>')
+      assert.equal($('body').html(), '<i>Howdy</i><i>Doody</i>')
+    })
+  })
+})
--- velement.js
+++ velement.js
@@ -1,12 +1,38 @@
 var VText = require('./vtext')
+var vdomToHtml = require('vdom-to-html')
+var htmlToVdom = require('html-to-vdom')({
+  VNode: require('virtual-dom/vnode/vnode'),
+  VText: require('virtual-dom/vnode/vtext')
+});
 
 function VElement(node) {
   this.tagName = node.tagName
   this.node = node
-  this.childNodes = node.children.map(function(child) {
+
+  overwriteChildNodes(this, node.children)
+
+  Object.defineProperty(this, 'innerHTML', {
+    set: function(html) {
+      overwriteChildNodes(this, [].concat(htmlToVdom(html)))
+    }.bind(this),
+    get: function() {
+      return this.node.children.map(function (child) {
+        return vdomToHtml(child) }).join('')
+      }.bind(this)
+  });
+
+  Object.defineProperty(this, 'lastChild', {
+    get: function() {
+      return this.childNodes[this.childNodes.length - 1]
+    }.bind(this)
+  });
+}
+
+function overwriteChildNodes(element, children) {
+  element.node.children = children
+  element.childNodes = children.map(function(child) {
     return child.type === 'VirtualText' ? new VText(child) : new VElement(child)
   })
-  this.lastChild = this.childNodes[this.childNodes.length - 1]
 }
 
 VElement.prototype.nodeType = 1
@@ -16,7 +42,8 @@ VElement.prototype.getElementsByTagName = function(tagName) {
   var queue = [].concat(this.childNodes)
   while (queue.length > 0) {
     var child = queue.shift()
-    if (child.nodeType == 1 && child.tagName.toLowerCase() == tagName.toLowerCase()) {
+    if (child.nodeType == 1 &&
+        child.tagName.toLowerCase() == tagName.toLowerCase()) {
       elements.push(child)
     }
     queue = queue.concat(child.childNodes || [])
